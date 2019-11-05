9064adb75b50e1b7ede505c07401515c1b5e6389
Bjornar97 <hetlest@hotmail.com>
2019-11-05 20:22:41 +0100

    Added features and made a dockerfile

--- bot.js
+++ bot.js
@@ -1,13 +1,31 @@
 const tmi = require("tmi.js");
-var admin = require('firebase-admin');
+var admin = require("firebase-admin");
 require("dotenv").config();
-const fs = require('fs');
+const fs = require("fs");
 var serviceAccount = require("./adminKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://rendogtv-viewers-bot.firebaseio.com"
 });
+
+// For docker to work...
+("use strict");
+
+const express = require("express");
+
+// Constants
+const PORT = 8085;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("The bot is running\n");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
 var db = admin.firestore();
 
@@ -22,8 +40,14 @@ const opts = {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN
   },
+  connection: {
+    secure: false,
+    reconnect: true
+  },
   channels: [process.env.CHANNEL_NAME]
 };
+
+console.dir(opts);
 
 // Create a client with our options
 const client = new tmi.client(opts);
@@ -37,6 +61,9 @@ client.connect();
 
 let first = true;
 let mcnames = {};
+
+let blameRenCount = 0;
+let badIdeaCount = 0;
 
 try {
   if (fs.existsSync("./mcnames.json")) {
@@ -54,7 +81,7 @@ function onMessageHandler(target, context, msg, self) {
     client.say(target, "/color green");
     first = false;
   }
-  
+
   if (self) {
     return;
   } // Ignore messages from the bot
@@ -74,18 +101,22 @@ function onMessageHandler(target, context, msg, self) {
   // If the command is known, let's execute it
   if (commandName === "!here") {
     if (context.subscriber) {
-      if (context["badge-info"].subscriber && context["badge-info"].subscriber != "") {
+      if (
+        context["badge-info"].subscriber &&
+        context["badge-info"].subscriber != ""
+      ) {
         const docref = db.collection("subs").doc(context["display-name"]);
-        docref.get().then((doc) => {
+        docref.get().then(doc => {
           let mcname = "";
-            if (mcnames[context["display-name"]]) {
-              mcname = mcnames[context["display-name"]].mcname;
-            }
+          if (mcnames[context["display-name"]]) {
+            mcname = mcnames[context["display-name"]].mcname;
+          }
           if (doc.exists) {
             docref.update({
               name: context["display-name"],
               months: context["badge-info"].subscriber,
-              will: commandText.charAt(0).toUpperCase() + commandText.substring(1),
+              will:
+                commandText.charAt(0).toUpperCase() + commandText.substring(1),
               timestamp: Date.now(),
               mcname: mcname
             });
@@ -93,13 +124,14 @@ function onMessageHandler(target, context, msg, self) {
             docref.set({
               name: context["display-name"],
               months: context["badge-info"].subscriber,
-              will: commandText.charAt(0).toUpperCase() + commandText.substring(1),
+              will:
+                commandText.charAt(0).toUpperCase() + commandText.substring(1),
               selected: false,
               timestamp: Date.now(),
               mcname: mcname
             });
           }
-        })
+        });
 
         registeredArray.push(context["display-name"]);
       }
@@ -119,23 +151,33 @@ function onMessageHandler(target, context, msg, self) {
           registeredArray = [];
           timeoutGoing = false;
           setTimeout(() => {
-            client.say(target, "If you should be in the list, but arent, please run the command again.");
+            client.say(
+              target,
+              "If you should be in the list, but arent, please run the command again."
+            );
           }, 2000);
-        }, 10000);
+        }, 20000);
       }
     }
     console.log(`* Executed ${commandName} command`);
   } else if (commandName === "!leave") {
-    db.collection("subs").doc(context["display-name"]).delete();
-
+    db.collection("subs")
+      .doc(context["display-name"])
+      .delete();
   } else if (commandName === "!mcname") {
     let name = commandArray[1];
     if (name) {
       const coll = db.collection("subs");
-      mcnames[context["display-name"]] = {twitchname: context["display-name"], mcname: name};
-      coll.doc(context["display-name"]).update({mcname: name}).catch((error) => {
-        console.log("Error whle adding mcname");
-      });
+      mcnames[context["display-name"]] = {
+        twitchname: context["display-name"],
+        mcname: name
+      };
+      coll
+        .doc(context["display-name"])
+        .update({ mcname: name })
+        .catch(error => {
+          console.log("Error whle adding mcname");
+        });
       if (!saveGoing) {
         saveGoing = true;
         setTimeout(() => {
@@ -144,11 +186,18 @@ function onMessageHandler(target, context, msg, self) {
         }, 5000);
       }
     } else {
-      client.say(target, `@${context["display-name"]} Usage: "!mcname <your mc-name>" This way you can tell rendog that your mc-name is different from your twitch name. Minecraft names are CASE SENSITIVE!`);
+      client.say(
+        target,
+        `@${
+          context["display-name"]
+        } Usage: "!mcname <your mc-name>" This way you can tell rendog that your mc-name is different from your twitch name. Minecraft names are CASE SENSITIVE!`
+      );
     }
   } else if (commandName === "!removemcname") {
     mcnames[context["display-name"]] = null;
-    db.collection("subs").doc(context["display-name"]).update({mcname: null});
+    db.collection("subs")
+      .doc(context["display-name"])
+      .update({ mcname: null });
     if (!saveGoing) {
       saveGoing = true;
       setTimeout(() => {
@@ -158,54 +207,127 @@ function onMessageHandler(target, context, msg, self) {
     }
   } else if (commandName === "!reset") {
     if (context.mod) {
-      db.collection("subs").get().then((snapshot) => {
-        snapshot.docs.forEach((doc) => {
-          db.collection("subs").doc(doc.id).delete();
+      db.collection("subs")
+        .get()
+        .then(snapshot => {
+          snapshot.docs.forEach(doc => {
+            db.collection("subs")
+              .doc(doc.id)
+              .delete();
+          });
         });
-      })
       client.say(target, "Reset registered");
     } else {
-      client.say(target, `@${context["display-name"]} You are not a mod, and does not have access to that command`);
+      client.say(
+        target,
+        `@${
+          context["display-name"]
+        } You are not a mod, and does not have access to that command`
+      );
     }
   } else if (commandName === "!remove") {
     if (!context.mod) {
-      client.say(`@${name} You are not a mod, and cannot use this command. To remove yourselft use !leave`);
+      client.say(
+        `@${name} You are not a mod, and cannot use this command. To remove yourselft use !leave`
+      );
     } else {
       let selectName = commandArray[1];
       if (selectName.split("")[0] == "@") {
         selectName = commandArray[1].substring(1);
       }
 
-      db.collection("subs").doc(selectName).delete();
+      db.collection("subs")
+        .doc(selectName)
+        .delete();
       client.say(`Deleted ${selectName}`);
+    }
+  } else if (commandName === "!blameren") {
+    blameRenCount += 1;
+    client.say(
+      target,
+      `Rendog has been blamed ${blameRenCount} times this stream`
+    );
+  } else if (commandName === "!resetblameren") {
+    if (context.mod) {
+      blameRenCount = 0;
+      client.say(
+        target,
+        `@${context["display-name"]} Blame ren has been reset`
+      );
+    }
+  } else if (commandName === "!badidea") {
+    badIdeaCount += 1;
+    client.say(target, `${badIdeaCount} people think that is a bad idea!`);
+  } else if (commandName === "!resetbadidea") {
+    if (context.mod) {
+      badIdeaCount = 0;
+      client.say(
+        target,
+        `@${context["display-name"]} Bad Idea Count has been reset`
+      );
     }
   } else if (commandName === "!dice") {
     const num = rollDice();
     client.say(target, `@${context["display-name"]} You rolled a ${num}`);
+  } else if (commandName === "!important") {
+    console.log(msg);
+
+    if (context.mod) {
+      const textArray = msg.split(" ");
+      textArray.shift();
+      const text = textArray.join(" ");
+      db.collection("important").add({
+        from: `@${context["display-name"]}`,
+        color: context.color,
+        text: text,
+        timestamp: Date.now()
+      });
+      console.log("Important message recieved");
+    }
+  } else if (commandName === "!resetimportant") {
+    if (context.mod) {
+      db.collection("important")
+        .get()
+        .then(docs => {
+          docs.forEach(doc => {
+            doc.ref.delete();
+          });
+        });
+    }
   } else if (commandName === "!how") {
-    client.say(target, `Here are the available commands for subs: 
+    client.say(
+      target,
+      `Here are the available commands for subs: 
                         | ¤ \"!here <action>\" to tell rendog you are in chat. Action is optional and can be \"fight\" or \"mine\"
                         | ¤ \"!leave\" so you dont get used
-                        | ¤ "!mcname <minecraft-name>" to tell rendog your minecraft-name is different from your twitch name. MC-NAMES ARE CASE SENSITIVE!`);
+                        | ¤ "!mcname <minecraft-name>" to tell rendog your minecraft-name is different from your twitch name. MC-NAMES ARE CASE SENSITIVE!`
+    );
   } else if (commandName === "!modhow") {
-    client.say(target, `Mods can use these commands: ¤ "!reset" to delete everyone from the list, use with caution | ¤ "!remove <name>" to remove a specific user from the list (you can use @)`);
+    client.say(
+      target,
+      `Mods can use these commands: 
+                    | ¤ "!reset" to delete everyone from the list, use with caution 
+                    | ¤ "!remove <name>" to remove a specific user from the list (you can use @)
+                    | ¤ "!important <message>" To inform rendog of something important. DO NOT ABUSE!
+                    `
+    );
   } else {
     console.log(`* Unknown command ${commandName}`);
-  } 
+  }
 }
 
 function writeMcNames(names) {
   const namesJSON = JSON.stringify(names);
-  fs.writeFile('./mcnames.json', namesJSON, err => {
+  fs.writeFile("./mcnames.json", namesJSON, err => {
     if (err) {
-        console.log('Error writing file', err)
+      console.log("Error writing file", err);
     } else {
-        console.log('Successfully wrote file')
+      console.log("Successfully wrote file");
     }
-})
+  });
 }
 
-function rollDice () {
+function rollDice() {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
